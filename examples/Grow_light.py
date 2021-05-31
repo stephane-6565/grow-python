62201150847a7ef5325a5986b9439b6ab6904aa9
Stephane Aparicio <83454647+stephane-6565@users.noreply.github.com>
2021-05-31 13:41:59 +0100

    Update Grow_light.py
    library update

--- examples/Grow_light.py
+++ examples/Grow_light.py
@@ -7,20 +7,19 @@
 
 import datetime
 import RPi.GPIO as GPIO
-from board import SCL, SDA
-from busio import I2C
-from pimoroni_circuitpython_ltr559 import Pimoroni_LTR559
+from ltr559 import LTR559
 
+ltr559 = LTR559()
 
-I2C_BUS = I2C(SCL, SDA)
-LTR559 = Pimoroni_LTR559(I2C_BUS)
 light_value = 200 # Random number, need to test that
 
 def main():
     now = datetime.datetime.now()
     
     while True:
-        LTR559.lux < light_value  # while Lux value from light sensor is bellow the requested light value 
+        ltr559.update_sensor()
+        lux = ltr559.get_lux()
+        lux < light_value         # while Lux value from light sensor is bellow the requested light value
         if 6 <= now.hour >= 20:   # only turn on light between 06h00 and 20h00.
             print("light on")     # Turn on light code (need to find a relay)
         else
