115246810279848f83c6338974198e0a49460b65
Stephane Aparicio <83454647+stephane-6565@users.noreply.github.com>
2021-06-03 11:48:01 +0100

    Update Grow_light.py
    fix if conditions

--- examples/Grow_light.py
+++ examples/Grow_light.py
@@ -11,7 +11,7 @@ from ltr559 import LTR559
 
 ## Variables
 ltr559 = LTR559() # Calling the light sensor
-light_value = 200 # Random number, need to test that
+light_value = 2000 # Random number, need to test that
 
 
 ## Main function
@@ -20,11 +20,10 @@ def main():
     while True:
         ltr559.update_sensor()
         lux = ltr559.get_lux()
-        lux < light_value         # while Lux value from light sensor is bellow the requested light value
-        if 6 <= now.hour >= 20:   # only turn on light between 06h00 and 20h00.
-            print("light on (lux = {})".format(lux))     # Turn on light (need to find a relay)
-        else
-            print("light off (lux = {})".format(lux))    # Turn off light (need to find a relay)
+        if 6 <= now.hour >= 20 and lux < light_value :   # only turn on light between 06h00 and 20h00 while Lux value from light sensor is bellow the requested light value.
+            print("Time: {a} | Light on (lux = {b})".format(a = now.hour , b = lux))     # Turn on light (need to find $        else:
+            print("Time: {a} | Light off (lux = {b})".format(a= now.hour , b = lux))    # Turn off light (need to find $
 
+   
 if __name__ == "__main__":
-    main()
+   main()
