f6ccace1a9ba5c6b8488aeb6f16c351533e9e6a4
Stephane Aparicio <83454647+stephane-6565@users.noreply.github.com>
2021-06-03 12:08:28 +0100

    Update Grow_light.py

--- examples/Grow_light.py
+++ examples/Grow_light.py
@@ -20,8 +20,9 @@ def main():
     while True:
         ltr559.update_sensor()
         lux = ltr559.get_lux()
-        if 6 <= now.hour >= 20 and lux < light_value :   # only turn on light between 06h00 and 20h00 while Lux value from light sensor is bellow the requested light value.
+        if (6 <= now.hour <= 20) and (lux < light_value) :   # only turn on light between 06h00 and 20h00 while Lux value from light sensor is bellow the requested light value.
             print("Time: {a} | Light on (lux = {b})".format(a = now.hour , b = lux))     # Turn on light (need to find $        else:
+        else:    
             print("Time: {a} | Light off (lux = {b})".format(a= now.hour , b = lux))    # Turn off light (need to find $
 
    
