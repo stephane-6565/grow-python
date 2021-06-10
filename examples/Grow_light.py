78e4e5c5dd040a1400e41e8b2f38976884342859
Stephane Aparicio <83454647+stephane-6565@users.noreply.github.com>
2021-06-10 16:05:45 +0100

    Update Grow_light.py
    Add relay control

--- examples/Grow_light.py
+++ examples/Grow_light.py
@@ -12,6 +12,7 @@ from ltr559 import LTR559
 ## Variables
 ltr559 = LTR559() # Calling the light sensor
 light_value = 2000 # Random number, need to test that
+gpio.setup(16, GPIO.OUT) # LED control
 
 
 ## Main function
@@ -21,9 +22,11 @@ def main():
         ltr559.update_sensor()
         lux = ltr559.get_lux()
         if (6 <= now.hour <= 20) and (lux < light_value) :   # only turn on light between 06h00 and 20h00 while Lux value from light sensor is bellow the requested light value.
-            print("Time: {a} | Light on (lux = {b})".format(a = now.hour , b = lux))     # Turn on light (need to find $        else:
+            print("Time: {a} | Light on (lux = {b})".format(a = now.hour , b = lux))     # Turn on light 
+            GPIO.output(16, True) 
         else:    
-            print("Time: {a} | Light off (lux = {b})".format(a= now.hour , b = lux))    # Turn off light (need to find $
+            print("Time: {a} | Light off (lux = {b})".format(a= now.hour , b = lux))    # Turn off light 
+            GPIO.output(16, False)
 
    
 if __name__ == "__main__":
